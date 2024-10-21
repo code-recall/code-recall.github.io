9fefe33e73781d6c05ff531c9808b0f5d290b45f
adrianrivers <adrianlrivers@gmail.com>
2024-10-21 18:11:23 +1300

    fix: fix base path issue rendering incorrect relative urls

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
-import mdx from '@astrojs/mdx';
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://code-recall.github.io',
-  base: 'code-recall/web',
   integrations: [mdx()],
 })

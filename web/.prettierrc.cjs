86cb02987db9c58579a3f234de8fc28f22e52ff7
adrianrivers <adrianlrivers@gmail.com>
2024-10-21 16:12:41 +1300

    feature: add astro

---
+++ web/.prettierrc.cjs
@@ -0,0 +1,15 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+};

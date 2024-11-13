008e1946a3d165bec7945815a0fe85779b809fbb
Adrian Rivers <adrian.rivers@clearpoint.co.nz>
2024-11-14 10:23:20 +1300

    feat: start blogging template

--- web/src/content/config.ts
+++ web/src/content/config.ts
@@ -1,9 +1,21 @@
-import { defineCollection } from 'astro:content'
+import { defineCollection, z } from 'astro:content'
 
-const readingList = defineCollection({
+export const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    author: z.string(),
+    tags: z.optional(z.array(z.string())),
+  }),
+})
+
+export const readingList = defineCollection({
   type: 'content',
 })
 
 export const collections = {
   readingList,
+  blog,
 }
